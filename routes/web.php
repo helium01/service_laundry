707293d353c65d714d50c00969066b0c27533b8b
Kaleb <molewekaleb33@gmail.com>
2023-04-18 15:01:32 +0700

    menambahkan server midtrans

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\midtranscontrol;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+route::get('/server',[midtranscontrol::class,'server']);
